e7ca4f8020197cd3dbca1160be881fa573df7e47
bejer <bejerdk@gmail.com>
2011-12-14 00:19:42 +0100

    Modified to better handle incomplete feature files

--- utility/parse_static_features_and_put_in_db.py
+++ utility/parse_static_features_and_put_in_db.py
@@ -4,6 +4,7 @@
 import sys
 import os.path
 import pymongo
+import re
 
 def extract_function_name(filename):
     return filename.split('.')[1]
@@ -22,16 +23,26 @@ features = file_content.split(',')
 
 static_features = {"project_name": project_name, "function_name": function_name, "extracted_after_pass": extracted_after_pass}
 
+include_this_function = True
+
 for s in features:
     s = s.strip(' \n')
-    [name, value] = s.split('=')
-    static_features[name] = value
+    # Found a single file with no content so far.... 
+    if re.search(r"ft\d+=[\d\.]+", s):
+        [name, value] = s.split('=')
+        static_features[name] = value
+    else:
+        print("{}: {}".format(file, s)) # Used for debugging files
+        include_this_function = False
+        break
 
 # Maybe the static features should just all be placed in one big collection instead of having one collection for each project?
-db_conn = pymongo.Connection("localhost", 27111)
-db = db_conn["static_features"]
-db_coll = db[project_name]
 
-db_coll.insert(static_features)
-
-db_conn.disconnect()
+if include_this_function:
+    db_conn = pymongo.Connection("localhost", 27111)
+    db = db_conn["static_features"]
+    db_coll = db[project_name]
+    
+    db_coll.insert(static_features)
+    
+    db_conn.disconnect()
